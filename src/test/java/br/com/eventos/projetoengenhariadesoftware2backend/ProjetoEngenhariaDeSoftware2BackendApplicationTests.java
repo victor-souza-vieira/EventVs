8aaddc06be4421f2759fbeaccf25007271e37776
victor souza <victor.ti.souza@gmail.com>
2021-04-24 18:15:04 -0300

    first commit

---
+++ src/test/java/br/com/eventos/projetoengenhariadesoftware2backend/ProjetoEngenhariaDeSoftware2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eventos.projetoengenhariadesoftware2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEngenhariaDeSoftware2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
