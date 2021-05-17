310cf015157f58ab704488057460902d28221416
victor souza <victor.ti.souza@gmail.com>
2021-05-16 23:39:03 -0300

    Codigo base

--- src/main/java/br/com/eventvs/domain/model/Produtor.java
+++ src/main/java/br/com/eventvs/domain/model/Produtor.java
@@ -3,30 +3,28 @@ package br.com.eventvs.domain.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario {
+public class Produtor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(length = 60, nullable = false)
     private String nome;
 
+    @Column(length = 11, nullable = false)
     private String cpf;
 
+    @Column(length = 60, nullable = false)
     private String email;
 
+    @Column(length = 60, nullable = true)
     private String senha;
-
-    private boolean flagParticipante;
-
 
 }
