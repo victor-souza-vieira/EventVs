168ba10f7654a388f084edcbaa540bab806ec80d
gabill <evelyngabill96@gmail.com>
2021-06-10 11:47:18 -0300

    Criacao dos Resources

---
+++ src/main/java/br/com/eventvs/api/resources/InscricaoResource.java
@@ -0,0 +1,10 @@
+package br.com.eventvs.api.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static br.com.eventvs.api.util.Paths.PATH_INSCRICAO;
+
+@RestController
+@RequestMapping(value = PATH_INSCRICAO)
+public class InscricaoResource {
+}
--- src/main/java/br/com/eventvs/api/util/Paths.java
+++ src/main/java/br/com/eventvs/api/util/Paths.java
@@ -9,5 +9,12 @@ package br.com.eventvs.api.util;
 public class Paths {
 
     public static final String PATH_EVENTO = "/eventos";
+    public static final String PATH_ADMINISTRADOR = "/administradores";
+    public static final String PATH_CATEGORIA = "/categorias";
+    public static final String PATH_ENDERECO = "/enderecos";
+    public static final String PATH_INSCRICAO = "/inscricoes";
+    public static final String PATH_PARTICIPANTE = "/participantes";
+    public static final String PATH_PESSOA = "/pessoas";
+    public static final String PATH_PRODUTOR = "/produtores";
 
 }
