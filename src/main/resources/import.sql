3e6dd4498be23936f864897ef5b1c8c575ff5843
gabill <evelyngabill96@gmail.com>
2021-06-10 12:28:50 -0300

    Listar Categoria

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-insert into categoria() values (null, "Palestra", "Simboliza um evento em formato de palestra");
+insert into categoria() values (null, "Simboliza um evento em formato de palestra", "Palestra");
+insert into categoria() values (null, "Simboliza um evento em formato de curso", "Curso");
