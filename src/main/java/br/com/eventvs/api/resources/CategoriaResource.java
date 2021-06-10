044a2ae25fc621c296bdde0772ec1cd4332e385d
gabill <evelyngabill96@gmail.com>
2021-06-10 12:37:05 -0300

    Adicionando ResponseStatus no metodo listarCategorias

--- src/main/java/br/com/eventvs/api/resources/CategoriaResource.java
+++ src/main/java/br/com/eventvs/api/resources/CategoriaResource.java
@@ -3,8 +3,10 @@ package br.com.eventvs.api.resources;
 import br.com.eventvs.domain.controller.CategoriaController;
 import br.com.eventvs.domain.model.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +21,7 @@ public class CategoriaResource {
     CategoriaController categoriaController;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Categoria> listarCategorias(){
         return categoriaController.listarCategorias();
     }
