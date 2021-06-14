f10b1d0f0f1312015b8659724d2a127a9b0ddc33
victor souza <victor.ti.souza@gmail.com>
2021-06-13 23:35:24 -0300

    Adicionando Refresh Token

--- src/main/java/br/com/eventvs/core/security/Authorization/AuthorizationServerConfig.java
+++ src/main/java/br/com/eventvs/core/security/Authorization/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.eventvs.core.security.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.
                 inMemory()
                     .withClient("app-mobile")
                         .secret(passwordEncoder.encode("mobile"))
-                        .authorizedGrantTypes("password")
+                        .authorizedGrantTypes("password", "refresh_token")
                         .scopes("write", "read")
-                        .accessTokenValiditySeconds(12 * 60 * 60)
+                        .accessTokenValiditySeconds(12 * 60 * 60) // 12 horas
+                        .refreshTokenValiditySeconds(10 * 24 * 60 * 60) // 10 dias
                     .and()
                         .withClient("checktoken")
                             .secret(passwordEncoder.encode("check123"));
@@ -42,6 +47,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
--- src/main/java/br/com/eventvs/core/security/Authorization/WebSecurityConfig.java
+++ src/main/java/br/com/eventvs/core/security/Authorization/WebSecurityConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,5 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
     }
 }
