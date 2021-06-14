e114034ab1f9dedd1084df04f94f41632f0b4502
victor souza <victor.ti.souza@gmail.com>
2021-06-14 17:04:24 -0300

    Adicionando Login via base de dados com token opaco

--- src/main/java/br/com/eventvs/core/security/Authorization/AuthorizationServerConfig.java
+++ src/main/java/br/com/eventvs/core/security/Authorization/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package br.com.eventvs.core.security.Authorization;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
--- src/main/java/br/com/eventvs/core/security/domain/AuthPessoa.java
+++ src/main/java/br/com/eventvs/core/security/domain/AuthPessoa.java
@@ -1,12 +1,16 @@
 package br.com.eventvs.core.security.domain;
 
 import br.com.eventvs.domain.model.Pessoa;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collections;
 
 public class AuthPessoa extends User {
 
+    @Getter
+    @Setter
     private String nome;
 
     public AuthPessoa(Pessoa pessoa){
