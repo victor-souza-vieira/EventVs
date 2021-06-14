14d56ccec93fb90504c01ba331a54a7622b5e0d2
victor souza <victor.ti.souza@gmail.com>
2021-06-14 15:08:12 -0300

    Removendo autenticacao in memory

--- src/main/java/br/com/eventvs/core/security/Authorization/WebSecurityConfig.java
+++ src/main/java/br/com/eventvs/core/security/Authorization/WebSecurityConfig.java
@@ -3,11 +3,9 @@ package br.com.eventvs.core.security.Authorization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,14 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("victor")
-                    .password(passwordEncoder().encode("123"))
-                    .roles("ADMIN");
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -33,11 +23,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
-    }
-
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        return super.userDetailsService();
     }
 }
