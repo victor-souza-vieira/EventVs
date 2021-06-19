782304f7a1cf6f31018c7a970427f55fa4c4c690
victor souza <victor.ti.souza@gmail.com>
2021-06-18 21:30:48 -0300

    Adicionando role no retorno do login

--- src/main/java/br/com/eventvs/domain/repository/ProdutorRepository.java
+++ src/main/java/br/com/eventvs/domain/repository/ProdutorRepository.java
@@ -1,9 +1,11 @@
 package br.com.eventvs.domain.repository;
 
+import br.com.eventvs.domain.model.Pessoa;
 import br.com.eventvs.domain.model.Produtor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutorRepository extends JpaRepository<Produtor,Integer> {
+    Produtor findByPessoa(Pessoa pessoa);
 }
