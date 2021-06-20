9878fdec98293500fdf77de10d992e4052c48eff
victor-souza-vieira <39530445+victor-souza-vieira@users.noreply.github.com>
2021-06-20 18:26:11 -0300

    Update GerenciarContaProdutor.java

--- src/main/java/br/com/eventvs/domain/controller/GerenciarContaProdutor.java
+++ src/main/java/br/com/eventvs/domain/controller/GerenciarContaProdutor.java
@@ -1,25 +1,7 @@
 package br.com.eventvs.domain.controller;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import br.com.eventvs.domain.model.Produtor;
-import br.com.eventvs.domain.repository.ProdutorRepository;
 
 @Service
 public class GerenciarContaProdutor {
-	
-	@Autowired
-	private ProdutorRepository produtorRespository;
-	
-	
-	/**
-	 * Método para verificar se exite um produtor cadastrado no banco com um determinado id
-	 * */
-	public boolean getById(Integer id) {
-		return produtorRespository.existsById(id);
-		
-	}
 }
