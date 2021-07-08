712707c1191990e5a9c3217edb2985d822006571
victor souza <victor.ti.souza@gmail.com>
2021-07-07 23:19:05 -0300

    Correcao no nome do metodo de login

--- src/main/java/br/com/eventvs/domain/controller/GerenciarContaController.java
+++ src/main/java/br/com/eventvs/domain/controller/GerenciarContaController.java
@@ -2,9 +2,11 @@ package br.com.eventvs.domain.controller;
 
 import br.com.eventvs.domain.exception.NegocioException;
 import br.com.eventvs.domain.model.Administrador;
+import br.com.eventvs.domain.model.Participante;
 import br.com.eventvs.domain.model.Pessoa;
 import br.com.eventvs.domain.model.Produtor;
 import br.com.eventvs.domain.repository.AdministradorRepository;
+import br.com.eventvs.domain.repository.ParticipanteRepository;
 import br.com.eventvs.domain.repository.PessoaRepository;
 import br.com.eventvs.domain.repository.ProdutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,9 @@ public class GerenciarContaController {
 
     @Autowired
     private AdministradorRepository administradorRepository;
+
+    @Autowired
+    private ParticipanteRepository participanteRepository;
 
     /**
      * Método responsável por verificar se uma pessoa realmente existe no banco de dados
@@ -46,6 +51,19 @@ public class GerenciarContaController {
     public Produtor loginProdutor(Pessoa pessoa){
         return produtorRepository.findByPessoaId(pessoa.getId()).orElseThrow(() -> {
             throw new NegocioException("Usuário não é um produtor de eventos.");
+        });
+    }
+
+    /**
+     * Método responsável por verificar se uma pessoa logada na api é um produtor
+     *
+     * @param pessoa
+     * @return Produtor
+     * @throws NegocioException {@link NegocioException}
+     * */
+    public Participante loginParticipante(Pessoa pessoa){
+        return participanteRepository.findByPessoaEmail(pessoa.getEmail()).orElseThrow(() -> {
+            throw new NegocioException("O usuário não é um participante.");
         });
     }
 
